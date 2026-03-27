a8fbaa717471f58f98ae00d1a25b43dd5811dd78
DeveloperSkeletor <pat.skelly@proton.me>
2026-03-27 12:47:25 -0400

    Task Management page

---
+++ task-management/src/components/Tasks.jsx
@@ -0,0 +1,21 @@
+import NewTask from "./NewTask";
+
+export default function Tasks({ tasks, onAdd, onDelete }) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-stone700 mb-4">Tasks</h2>
+      <NewTask onAdd={onAdd}></NewTask>
+      {tasks.lengh === 0 && <p className="text-stone-800 mb-4">No Tasks Yet</p>}
+      {tasks.lengh > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <buton className="text-stone-700 hover:text-red-500">Clear</buton>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
